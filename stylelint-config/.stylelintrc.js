bf15a74d431bd8749733cec6fcd0f2e3ec0f2c07
Dries Beerten <dries.beerten@kunstmaan.be>
2018-08-21 11:07:39 +0200

    add stylelint scss plugin

--- stylelint-config/.stylelintrc.js
+++ stylelint-config/.stylelintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "plugins": [
-        "stylelint-order"
+        "stylelint-order",
+        "stylelint-scss"
     ],
     "extends": "stylelint-config-standard",
     "rules": {
@@ -35,9 +36,7 @@ module.exports = {
                 "ignore": ["after-comment"]
             }
         ],
-        "at-rule-no-unknown": [ true, {
-            "ignoreAtRules": ["extend", "at-root", "debug", "warn", "error", "if", "else", "for", "each", "while", "mixin", "include", "content", "return", "function"]
-        }],
+        "at-rule-no-unknown": null,
         "max-nesting-depth": 7,
         "no-descending-specificity": null,
         "order/properties-order": [
@@ -206,6 +205,32 @@ module.exports = {
                     "perspective"
                 ]
             }
+        ],
+        "scss/at-else-closing-brace-newline-after": "always-last-in-chain",
+        "scss/at-else-closing-brace-space-after": "always-intermediate",
+        "scss/at-else-empty-line-before": "never",
+        "scss/at-else-if-parentheses-space-before": "always",
+        "scss/at-extend-no-missing-placeholder": true,
+        "scss/at-if-closing-brace-newline-after": "always-last-in-chain",
+        "scss/at-if-closing-brace-space-after": "always-intermediate",
+        "scss/at-function-parentheses-space-before": "never",
+        "scss/at-import-no-partial-leading-underscore": true,
+        "scss/at-mixin-argumentless-call-parentheses": "always",
+        "scss/at-mixin-named-arguments": "always",
+        "scss/at-mixin-parentheses-space-before": "never",
+        "scss/at-rule-no-unknown": true,
+        "scss/dollar-variable-colon-newline-after": "always-multi-line",
+        "scss/dollar-variable-colon-space-after": "at-least-one-space",
+        "scss/dollar-variable-colon-space-before": "never",
+        "scss/dollar-variable-no-missing-interpolation": true,
+        "scss/double-slash-comment-whitespace-inside": "always",
+        "scss/declaration-nested-properties": "never",
+        "scss/operator-no-newline-after": true,
+        "scss/operator-no-newline-before": true,
+        "scss/operator-no-unspaced": true,
+        "scss/selector-no-redundant-nesting-selector": true,
+        "scss/no-duplicate-dollar-variables": [,
+            { "ignoreInsideAtRules": ["if", "mixin"] }
         ]
     }
 }
